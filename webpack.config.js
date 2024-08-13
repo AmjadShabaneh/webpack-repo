660578b5436907115e3ca1084cbcb51bad1a32c9
Amjad Shabaneh <213510@ppu.edu.ps>
2024-08-13 15:14:53 +0300

    fsafaf

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,12 @@ module.exports = {
                 loader: "babel-loader"
             }
         ]
-    }
+    },
+    plugins:[ 
+        new HtmlWebPackPlugin({
+            template: './src/client/views/index.html',
+            filename:'./index.html'
+    })
+    ]
+   
 }
